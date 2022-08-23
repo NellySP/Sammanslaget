c74a9046f8f5a41f2fb13c341f2641798e4e410d
NellySP <nelly.petren@gmail.com>
2022-08-23 10:26:18 +0200

    add example image
    and about page grid

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,50 @@
 import styled from "styled-components";
+import example from "../../images/glasses-pug.jpg";
 
-const StyledAbout = styled.div``;
+const StyledAbout = styled.div`
+  width: 100vw;
+  min-height: 100vh;
+
+  img {
+    width: 70%;
+  }
+
+  p {
+    margin-right: 5vw;
+    margin-left: 5vw;
+  }
+
+  .parent {
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    grid-template-rows: repeat(3, 1fr);
+  }
+
+  .item {
+    border: 1px solid black;
+    margin: 3%;
+  }
+
+  a {
+    color: black;
+    text-decoration: underline;
+  }
+
+  a:hover {
+    text-decoration: underline black;
+  }
+
+  @media screen and (max-width: 600px) {
+    .parent {
+      display: flex;
+      flex-direction: column;
+    }
+
+    .item {
+      margin: 5%;
+    }
+  }
+`;
 
 const AboutPage = () => {
   return (
@@ -8,6 +52,38 @@ const AboutPage = () => {
       <div>
         <h2>About page</h2>
         <p>This space is all about the amazing us</p>
+      </div>
+      <div className="parent">
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
+        <div className="item">
+          <h3>Person</h3>
+          <img alt="This is dog" src={example}></img>
+          <p>Om person. oj så intressant!</p>
+        </div>
       </div>
     </StyledAbout>
   );
