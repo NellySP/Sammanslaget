0931d0792ca05d374b40227fbbd80bbb39cb48e1
NellySP <nelly.petren@gmail.com>
2022-08-25 10:09:28 +0200

    add informative text
    and a bit of styling

--- src/components/MobileGame/index.js
+++ src/components/MobileGame/index.js
@@ -1,15 +1,26 @@
 import styled from "styled-components";
 
 const StyledMobile = styled.div`
+  .container {
+    margin: 5%;
+    background-color: pink;
+    border-radius: 25px;
+  }
   p {
     font-family: "roboto";
+  }
+  h2 {
+    font-family: "Montserrat";
   }
 `;
 
 const MobileHome = () => {
   return (
     <StyledMobile>
-      <p>Använd en laptop för att spela spelet!</p>
+      <div className="container">
+        <h2>Så här kan vi inte ha det!</h2>
+        <p>Använd en laptop för att spela spelet!</p>
+      </div>
     </StyledMobile>
   );
 };
